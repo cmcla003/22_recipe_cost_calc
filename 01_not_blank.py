40efb446a90885bbaa65624d59257ff34964a983
cmcla003 <mcl@melville-high.school.nz>
2022-11-07 14:11:26 +1300

    added isdigit so no numbers accepted

--- 01_not_blank.py
+++ 01_not_blank.py
@@ -1,15 +1,21 @@
 
-def not_blank(question,error):
+def not_blank(question):
     valid = False
     while not valid:
         # ask user for entry and assesses if not blank
         response = input(question).strip().lower()
-        if response != "":
+        if response.isdigit():
+            print("Entry cannot have numbers please try again")
+            valid = False
+        elif response != "":
             return response
             valid = True
+
         else:
-            print(error)
+            print("Entry cannot be blank")
 
 
-recipe_name = not_blank("What is the name of your recipe?", "This can't be blank" )
-recipe_source = not_blank("Where did you get your recipe from?", "This cannot be blank")
+recipe_name = not_blank("What is the name of your recipe?")
+recipe_source = not_blank("Where did you get your recipe from?")
+
+print("You chose {} from {}".format(recipe_name,recipe_source))
