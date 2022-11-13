a14072ca5494fefb1eb7757e11d32c0e58c0f4ab
cmcla003 <mcl@melville-high.school.nz>
2022-11-14 08:37:39 +1300

    added not blank function

--- 07 unit list.py
+++ 07 unit list.py
@@ -1,8 +1,33 @@
+# not blank and checks for digit user entry
+def not_blank(question,number_ok):
+    error = "Cannot be blank"
+    digi_error = "Cannot have numbers"
+    valid = False
+    while not valid:
+        # ask user for entry and assesses if not blank
+        response = input(question).strip().lower()
+        has_errors = ""
+
+        if number_ok != "yes":
+            for letter in response:
+                if letter.isdigit() == True:
+                    has_errors = "yes"
+                    break
+
+        if response == "":
+            print(error)
+            continue
+        if has_errors != "":
+            print(digi_error)
+        else:
+            return response
+            valid = True
+
 keep_going =""
 while keep_going =="":
     recipe_unit = []
 
-    unit_to_check = input("Unit: ")
+    unit_to_check = not_blank("Unit: ","no")
 
     valid_units=[["t", "tsp", "teaspoon", "teaspoons"],
                  ["T", "tbsp", "tablespoon", "tablespoons"],
